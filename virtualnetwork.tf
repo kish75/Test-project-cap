465f8f9ef6434b69dbba4629495dee8b1ec18f31
kish75 <kishoreroyal4225@gmai.com>
2025-02-13 13:46:55 +0530

    added dependson

--- virtualnetwork.tf
+++ virtualnetwork.tf
@@ -3,5 +3,6 @@ name = "var.vnetname"
     resource_group_name = var.rgname
     location = var.rglocation
     address_space = var.addresspace
+    depends_on = [ azurerm_resource_group.Cap ]
   
 }
