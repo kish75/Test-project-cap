12f812faf70194f7778a573b4e07cbb3f7f0d91a
kish75 <kishoreroyal4225@gmai.com>
2025-02-13 12:20:11 +0530

    first commit

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "rgname" {
+    type = string
+    default = "test-project-cap"
+    description =  " Name of the RG"
+  
+}
+
+variable "rglocation" {
+    
+    default = " west us"
+    description = "Name of the RG Location"
+  
+}
