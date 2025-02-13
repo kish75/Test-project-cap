f1f1e6c3430ac2bf1b5c3d1484b25a5bbbb59e2c
kish75 <kishoreroyal4225@gmai.com>
2025-02-13 13:34:17 +0530

    vnet commit

--- virtualnetwork.tf
+++ virtualnetwork.tf
@@ -2,6 +2,6 @@ resource "azurerm_virtual_network" "Capvnet" {
 name = "var.vnetname"
     resource_group_name = var.rgname
     location = var.rglocation
-    address_space = "var.addresspace"
+    address_space = var.addresspace
   
 }
